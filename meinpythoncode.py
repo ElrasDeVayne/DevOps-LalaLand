40a5c2733c3d2031828954c2cb66845c81654e66
aberweger <145767058+aberweger@users.noreply.github.com>
2024-03-11 21:19:12 +0100

    Update von Annina

--- meinpythoncode.py
+++ meinpythoncode.py
@@ -1,7 +1,5 @@
 languages = ["C", "C++", "Perl", "Python"] 
 for x in languages:
     print("Dieser Code ist witzig")
-    print (x)
-    print(x + 1)
     print("Hier ist ein weitere Codebeispielgit ")
 
