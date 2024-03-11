81e9ccec80a6edc45362e859a6ccdd23e1a99f78
Kreshnik Sadriu <kreshnik@sadriu.ch>
2024-03-11 21:08:39 +0100

    Konfliktprovokation

--- meinpythoncode.py
+++ meinpythoncode.py
@@ -1,1 +1,7 @@
-print ("hello world")
+languages = ["C", "C++", "Perl", "Python"] 
+for x in languages:
+    print("Dieser Code ist witzig")
+    print (x)
+    print(x + 1)
+    print("Hier ist ein weitere Codebeispielgit ")
+
