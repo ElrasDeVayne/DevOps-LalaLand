16e307484c2cca7d59a6581911d18ce72387677e
Kreshnik Sadriu <kreshnik@sadriu.ch>
2024-03-11 21:37:28 +0100

    weiterer commit

--- meinpythoncode.py
+++ meinpythoncode.py
@@ -3,9 +3,5 @@ for x in languages:
     print("Dieser Code ist witzig")
     print("Hier ist ein weitere Codebeispielgit ")
     print("Hier geht es immernoch weiter weil print x noch funktioniert")
-    print("Hier ist ein weitere Codebeispielgit Teil2 ")
-    print("Hier geht es immernoch weiter weil print x noch funktioniert Teil3")
-
-
-
+    print(x)
 
