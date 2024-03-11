28d8960cc019c456ee7b3eae85f9c0704ff9dc06
aberweger <145767058+aberweger@users.noreply.github.com>
2024-03-11 21:38:31 +0100

    neuestes Update von Annina

--- meinpythoncode.py
+++ meinpythoncode.py
@@ -3,4 +3,5 @@ for x in languages:
     print("Dieser Code ist witzig")
     print("Hier ist ein weitere Codebeispielgit ")
     print("Hier geht es immernoch weiter weil print x noch funktioniert")
+    print("Achtung: neue Anpassung")
 
